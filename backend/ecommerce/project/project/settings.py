139a008da4c8d71ac561f6bd1f9d0332066a15c6
VarshaPrabhakaran748 <varshacs.nmr@gmail.com>
2026-05-09 21:20:29 +0530

    Switch to PostgreSQL

--- backend/ecommerce/project/project/settings.py
+++ backend/ecommerce/project/project/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import django
+
 import os
-from django.utils.encoding import force_str
-django.utils.encoding.force_text = force_str
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,10 +139,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
