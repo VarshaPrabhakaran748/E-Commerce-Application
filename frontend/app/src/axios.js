7319d97a3bb3acf47d3442b227d5a4897b7da834
VarshaPrabhakaran748 <varshacs.nmr@gmail.com>
2026-05-10 08:02:25 +0530

    fix: axios config

--- frontend/app/src/axios.js
+++ frontend/app/src/axios.js
@@ -4,4 +4,5 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
 });
 
+console.log("API URL:", process.env.REACT_APP_API_URL);
 export default instance;
